7a5225ae4e9fbbe154759dbd163bce505c646f75
vincent <2268317148@qq.com>
2019-05-01 13:17:13 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
-const App: React.FC = () => {
+export default function APP() {
+    useEffect(() => {
+
+    });
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
                 <p>
                      Edit <code>src/App.tsx</code> and save to reload.
                 </p>
@@ -22,5 +22,3 @@ const App: React.FC = () => {
         </div>
     );
 }
-
-export default App;
